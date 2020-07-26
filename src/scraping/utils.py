03d8d6258159b33460d5cd92f45ee5051fad02c1
Женя <jeka.mixaylov58@gmail.com>
2020-07-26 20:06:42 +0300

    second

---
+++ src/scraping/utils.py
@@ -0,0 +1,42 @@
+cyrilic_latters = {
+    'а': 'a',
+    'б': 'b',
+    'в': 'v',
+    'г': 'g',
+    'д': 'd',
+    'е': 'e',
+    'ё': 'e',
+    'ж': 'zh',
+    'з': 'z',
+    'и': 'i',
+    'й': 'y',
+    'к': 'k',
+    'л': 'l',
+    'м': 'm',
+    'н': 'n',
+    'о': 'o',
+    'п': 'p',
+    'р': 'r',
+    'с': 's',
+    'т': 't',
+    'у': 'y',
+    'ф': 'f',
+    'х': 'h',
+    'ц': 'ts',
+    'ч': 'ch',
+    'ш': 'sh',
+    'щ': 'sch',
+    'ъ': '',
+    'ы': 'y',
+    'ь': '',
+    'э': 'e',
+    'ю': 'yu',
+    'я': 'ya'
+}
+
+def from_cyrillic_to_eng(text: str):
+    text = text.replace(' ', '_').lower()
+    tmp = ''
+    for ch in text:
+        tmp += cyrilic_latters.get(ch, ch)
+    return tmp
